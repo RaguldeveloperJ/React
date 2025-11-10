a7aaa1c46d6248dbd2e1bd6f10d634cead9d8575
Ragul <jkrdragul2018@gmail.com>
2025-11-10 14:53:30 +0530

    commit

--- src/components/Useeffect.jsx
+++ src/components/Useeffect.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 const Useeffect = () => {
   let [count, setCount] = useState(0);
   useEffect(() => {
-    if (count >= 5) {
+    if (count > 5) {
       alert("Count is 5");
     }
   }, [count]);
